508c98df4b994933deebd0c3ff42546b5d839cba
YurijKuznetsov <super.ura103@yandex.ru>
2023-05-04 16:39:37 +0300

    Домашнее задание 2 спринта 8.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonMedia = findViewById<Button>(R.id.mediateka)
+        val buttonSetting = findViewById<Button>(R.id.setting)
+        val searchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@MainActivity, "нажали на Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(searchClickListener)
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this, "нажали на Медиатеку", Toast.LENGTH_SHORT).show()
+        }
+        buttonSetting.setOnClickListener {
+            Toast.makeText(this, "нажали на Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
