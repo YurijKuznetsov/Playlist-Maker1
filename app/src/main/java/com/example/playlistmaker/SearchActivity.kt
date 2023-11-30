07047a087936bd819cb7b4575cce577195c09542
YurijKuznetsov <super.ura103@yandex.ru>
2023-11-30 10:57:31 +0300

    Спринт 10

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,14 +1,16 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+
 
 class SearchActivity : AppCompatActivity() {
     companion object {
@@ -21,6 +23,17 @@ class SearchActivity : AppCompatActivity() {
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clear_icon)
         val toolbarSearch = findViewById<Toolbar>(R.id.toolbar_search)
+        val recyclerviewSearch = findViewById<RecyclerView>(R.id.recyclerview_search)
+        val searchAdapter = SearchAdapter(
+            listOf(
+                Track(getString(R.string.track_name_1), getString(R.string.artist_name_1), getString(R.string.track_time_1), getString(R.string.artwork_url_1)),
+                Track(getString(R.string.track_name_2), getString(R.string.artist_name_2), getString(R.string.track_time_2), getString(R.string.artwork_url_2)),
+                Track(getString(R.string.track_name_3), getString(R.string.artist_name_3), getString(R.string.track_time_3), getString(R.string.artwork_url_3)),
+                Track(getString(R.string.track_name_4), getString(R.string.artist_name_4), getString(R.string.track_time_4), getString(R.string.artwork_url_4)),
+                Track(getString(R.string.track_name_5), getString(R.string.artist_name_5), getString(R.string.track_time_5), getString(R.string.artwork_url_5))
+            )
+        )
+        recyclerviewSearch.adapter = searchAdapter
         toolbarSearch.setNavigationOnClickListener {
             finish()
         }
---
+++ app/src/main/java/com/example/playlistmaker/SearchViewHolder.kt
@@ -0,0 +1,34 @@
+package com.example.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+
+class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    private val trackName: TextView = itemView.findViewById(R.id.track_name)
+    private val artistName: TextView = itemView.findViewById(R.id.artist_name)
+    private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+    private val trackImage: ImageView = itemView.findViewById(R.id.track_image)
+
+    fun bind(model: Track) {
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+        val cornerRadius = itemView.resources.getDimension(R.dimen.padding_02)
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .centerCrop()
+            .transform(RoundedCorners(cornerRadius.toInt()))
+            .placeholder(R.drawable.placeholder)
+            .into(trackImage)
+    }
+
+
+}
+
+
+
