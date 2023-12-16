1c754f04f2d35810143dadc3726f7603246d43bd
YurijKuznetsov <super.ura103@yandex.ru>
2023-12-16 16:00:13 +0300

    Спринт 11

--- app/src/main/java/com/example/playlistmaker/SearchAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/SearchAdapter.kt
@@ -4,9 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class SearchAdapter(
-    private val tracks: List<Track>
-) : RecyclerView.Adapter<SearchViewHolder>(){
+class SearchAdapter : RecyclerView.Adapter<SearchViewHolder>(){
+    var tracks = ArrayList<Track>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track, parent, false)
